05efc1d094cd8e4444653bdad7477f64c7013c0b
Joshua Whitney <jwhitney114@fullsail.edu>
2013-05-31 17:25:06 -0400

    Final Project
    This is a new Repo. Original Commits are under Whitney_Joshua_Project2

---
+++ js/json.js
@@ -0,0 +1,53 @@
+//JSON data file
+//Joshua Whitney
+//VFW 1305
+//Assignment 4
+
+var json = {
+    "zombieTemp1": {
+        "fname": ["First Name:", "Josh"],
+        "lname": ["Last Name:", "Whitney"],
+        "email": ["Email Address:", "jwhitney@gmail.com"],
+        "tele":  ["Telephone Number:", "123-456-7890"],
+        "zname": ["Zombies First Name:", "Ralph"],
+        "zlast": ["Zombies Last Name:", "Brainstein"],
+        "sex":   ["Zombies Sex:", "Male"],
+        "prior": ["Priority:", "YES"],
+        "weap":  ["Weapon of Choice:", "Grenade"],
+        "write": ["Users Reason:", "Just Because"],
+        "rate":  ["User Rating:", "10"],
+        "date":  ["You Want Them Killed on:", "June 3, 2013"],
+        "inv":   ["Customer Reputation:", "Low"]
+    },
+    "zombieTemp2": {
+        "fname": ["First Name:", "Mike"],
+        "lname": ["Last Name:", "Howe"],
+        "email": ["Email Address:", "howeinsane@gmail.com"],
+        "tele":  ["Telephone Number:", "123-456-7890"],
+        "zname": ["Zombies First Name:", "bob"],
+        "zlast": ["Zombies Last Name:", "armLicker"],
+        "sex":   ["Zombies Sex:", "Male"],
+        "prior": ["Priority:", "NO"],
+        "weap":  ["Weapon of Choice:", "Crowbar"],
+        "write": ["Users Reason:", "hes an idiot!"],
+        "rate":  ["User Rating:", "4"],
+        "date":  ["You Want Them Killed on:", "July 18, 2013"],
+        "inv":   ["Customer Reputation:", "Medium"]
+    },
+    "zombieTemp3": {
+        "fname": ["First Name:", "Louise"],
+        "lname": ["Last Name:", "Whitney"],
+        "email": ["Email Address:", "lwhitney@gmail.com"],
+        "tele":  ["Telephone Number:", "123-456-7890"],
+        "zname": ["Zombies First Name:", "jina"],
+        "zlast": ["Zombies Last Name:", "toeTaster"],
+        "sex":   ["Zombies Sex:", "Female"],
+        "prior": ["Priority:", "YES"],
+        "weap":  ["Weapon of Choice:", "Bazooka"],
+        "write": ["Users Reason:", "Shes crazy!"],
+        "rate":  ["User Rating:", "6"],
+        "date":  ["You Want Them Killed on:", "August 10, 2013"],
+        "inv":   ["Customer Reputation:", "High"]
+    }
+    
+}
